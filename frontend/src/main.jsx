d3b795d5b06780220afe0a77fc09dc135a4f4286
Brian Almazo <brianalmazo@mac.mynetworksettings.com>
2025-10-02 19:44:51 -0400

    Started the front end

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import { Provider } from './components/ui/provider'
+import App from './App.jsx'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </StrictMode>,
+)
